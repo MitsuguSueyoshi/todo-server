95cc9259d36a4ae3a5073fbc5863599c0c7bbe8e
Mitsugu Sueyoshi <42401026+MitsuguSueyoshi@users.noreply.github.com>
2021-10-10 11:10:06 +0900

    gofmt.goimportsの導入

--- Makefile
+++ Makefile
@@ -1,4 +1,15 @@
 .PHONY: protoc
-protoc: ## protobufコードの生成
+protoc: ## gRPCのstubコードの生成
 	# protoc
 	docker-compose run --rm --entrypoint sh protoc ./scripts/protoc.sh
+
+	# gofmt,goimportsで整形
+	gofmt -s -w pkg/domain/proto/
+	gofmt -s -w protobuf/
+	goimports -w -local "github.com/todo-server" pkg/domain/proto/
+
+.PHONY: fmt
+fmt:
+	# gofmt,goimportsで整形
+	gofmt -s -w cmd/ pkg/
+	goimports -w -local "github.com/todo-server" cmd/ pkg/
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "log"
-    "net"
+	"log"
+	"net"
 
 	"google.golang.org/grpc"
 )
 
 const (
-    port = ":50051"
+	port = ":50051"
 )
 
 func main() {
 	// リッスン処理
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
+		log.Fatalf("failed to listen: %v", err)
+	}
 
 	// サーバー起動
 	s := grpc.NewServer()
@@ -24,4 +24,3 @@ func main() {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
-
