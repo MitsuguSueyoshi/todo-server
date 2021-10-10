8a7af78bd2c785b75f40655da329f7ce95641da1
Mitsugu Sueyoshi <42401026+MitsuguSueyoshi@users.noreply.github.com>
2021-10-10 10:36:07 +0900

    script修正

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -2,7 +2,7 @@
 
 proto_file_dir=./protobuf
 api_proto_files=$(find ${proto_file_dir}/api -type f -name '*.proto')
-api_out_dir=./pkg/domain/proto/api
+proto_out_dir=./pkg/domain/proto/api
 
 # If the paths=source_relative flag is specified,
 # the output file is placed in the same relative directory as the input file.
@@ -10,7 +10,7 @@ api_out_dir=./pkg/domain/proto/api
 
 protoc \
   -I=${proto_file_dir} \
-  --go_out=paths=source_relative:${api_out_dir} \
-  --go-grpc_out=paths=source_relative,require_unimplemented_servers=false:${api_out_dir} \
-  --grpc-gateway_out=paths=source_relative,logtostderr=true:${api_out_dir} \
+  --go_out=paths=source_relative:${proto_out_dir} \
+  --go-grpc_out=paths=source_relative,require_unimplemented_servers=false:${proto_out_dir} \
+  --grpc-gateway_out=paths=source_relative,logtostderr=true:${proto_out_dir} \
   ${api_proto_files};
